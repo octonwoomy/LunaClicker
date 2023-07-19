ba74c7ee3c738569034f2eac1d8d113b74a3a736
Luna Ramirez <rickharrison422@gmail.com>
2023-07-18 22:18:03 -0500

    first upgrade finished

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const upgradeButton = document.getElementById("upgrades");
 const upgradePage = document.getElementById("upgrades-page");
 const upgradeDouble = document.getElementById("double");
 const backButton = document.getElementById("back");
+let Buyable = true;
+let counterIncrement = 1;
 let counter = 0;
 
 function AddCounter(){
-    counter += 1;
+    counter += counterIncrement;
     counterText.textContent = `Times Clicked: ${counter}`;
 }
 
@@ -17,7 +19,15 @@ function DisplayUpgrades(){
 function EndUDisplay(){
     upgradePage.style.display = "none";
 }
+function BuyDouble() {
+    if (counter >= 50 && Buyable === true) {
+        counter -= 50;
+        counterIncrement *= 2;
+        Buyable = false;
+    }
+}
 
+upgradeDouble.addEventListener("click", BuyDouble)
 backButton.addEventListener("click", EndUDisplay)
 upgradeButton.addEventListener("click", DisplayUpgrades)
 button.addEventListener("click", AddCounter)
