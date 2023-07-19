787ed9916f2f062731cc27054ad8b9a4f303234c
Luna Ramirez <rickharrison422@gmail.com>
2023-07-19 01:42:30 -0500

    to finish last "upgrade" and fix vanity

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ const upgradeButton = document.getElementById("upgrades");
 const upgradePage = document.getElementById("upgrades-page");
 const upgradeDouble = document.getElementById("double");
 const backButton = document.getElementById("back");
-const UpgradeTriple = document.getElementById("triple")
+const UpgradeTriple = document.getElementById("triple");
+const UpgradeQuad = document.getElementById("quad");
+const UpgradePenta = document.getElementById("penta");
+const UpgradeHexa = document.getElementById("hexa");
+const nuke = document.getElementById("nuke")
 let Buyable1 = true;
 let Buyable2 = true;
+let Buyable3 = true;
+let Buyable4 = true;
+let Buyable5 = true;
+let Buyable6 = true;
+let Buyable7 = true;
 let counterIncrement = 1;
 let counter = 0;
 
@@ -30,6 +39,9 @@ function BuyDouble() {
         counterIncrement *= 2;
         Buyable1 = false;
     }
+    else if(Buyable1 === false){
+        upgradeDouble.style.backgroundColor = "black"
+    }
     else{
         alert("You're broke lol")
     }
@@ -42,11 +54,81 @@ function BuyTriple(){
         counterIncrement *= 3;
         Buyable2 = false;
     }
+    else if(Buyable2 === false){
+        UpgradeTriple.style.backgroundColor = "black"
+    }
     else{
         alert("You're broke lol")
     }
 }
 
+function BuyQuad(){
+    if (counter >= 3000 && Buyable3 === true) {
+        counter -= 3000;
+        counterText.textContent = `Times Clicked: ${counter}`;
+        counterIncrement *= 4;
+        Buyable3 = false;
+    }
+    else if(Buyable3 === false){
+        UpgradeQuad.style.backgroundColor = "black"
+    }
+    else{
+        alert("You're broke lol")
+    }
+}
+
+function BuyPenta(){
+    if (counter >= 10000 && Buyable4 === true) {
+        counter -= 10000;
+        counterText.textContent = `Times Clicked: ${counter}`;
+        counterIncrement *= 5;
+        Buyable4 = false;
+    }
+    else if(Buyable4 === false){
+        UpgradePenta.style.backgroundColor = "black"
+    }
+    else{
+        alert("You're broke lol")
+    }
+}
+
+function BuyHexa(){
+    if (counter >= 50000 && Buyable5 === true) {
+        counter -= 50000;
+        counterText.textContent = `Times Clicked: ${counter}`;
+        counterIncrement *= 6;
+        Buyable5 = false;
+    }
+    else if(Buyable5 === false){
+        UpgradeHexa.style.backgroundColor = "black"
+    }
+    else{
+        alert("You're broke lol")
+    }
+}
+
+function EndGame(){
+    if (counter >= 1000000 && Buyable6 === true) {
+        counter -= 1000000;
+        counterText.textContent = `Times Clicked: ${counter}`;
+        counterIncrement *= 4;
+        Buyable6 = false;
+    }
+    else if(Buyable6 === false){
+        UpgradeQuad.style.backgroundColor = "black"
+    }
+    else{
+        alert("You're broke lol")
+    }
+}
+
+
+
+
+UpgradeHexa.addEventListener("click", BuyHexa)
+UpgradePenta.addEventListener("click", BuyPenta)
+nuke.addEventListener("click", EndGame)
+UpgradeQuad.addEventListener("click", BuyQuad)
 UpgradeTriple.addEventListener("click", BuyTriple)
 upgradeDouble.addEventListener("click", BuyDouble)
 backButton.addEventListener("click", EndUDisplay)
