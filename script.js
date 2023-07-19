4f94ee009e0e3ed6c600037cf1afb7361314875e
Luna Ramirez <rickharrison422@gmail.com>
2023-07-19 00:55:39 -0500

    implemented 2nd upgrade

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const upgradeButton = document.getElementById("upgrades");
 const upgradePage = document.getElementById("upgrades-page");
 const upgradeDouble = document.getElementById("double");
 const backButton = document.getElementById("back");
-let Buyable = true;
+const UpgradeTriple = document.getElementById("triple")
+let Buyable1 = true;
+let Buyable2 = true;
 let counterIncrement = 1;
 let counter = 0;
 
@@ -20,14 +22,30 @@ function EndUDisplay(){
     upgradePage.style.display = "none";
 }
 function BuyDouble() {
-    if (counter >= 50 && Buyable === true) {
+    if (counter >= 50 && Buyable1 === true) {
         counter -= 50;
         counterText.textContent = `Times Clicked: ${counter}`;
         counterIncrement *= 2;
-        Buyable = false;
+        Buyable1 = false;
+    }
+    else{
+        alert("You're broke lol")
+    }
+}
+function BuyTriple(){
+    if (counter >= 300 && Buyable2 === true) {
+        counter -= 300;
+        counterText.textContent = `Times Clicked: ${counter}`;
+        counterIncrement *= 3;
+        Buyable2 = false;
+    }
+    else{
+        alert("You're broke lol")
     }
 }
 
+
+UpgradeTriple.addEventListener("click", BuyTriple)
 upgradeDouble.addEventListener("click", BuyDouble)
 backButton.addEventListener("click", EndUDisplay)
 upgradeButton.addEventListener("click", DisplayUpgrades)
